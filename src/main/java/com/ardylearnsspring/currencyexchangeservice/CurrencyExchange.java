040c289c0fb0a3a6bac13e855df0c15b598cc25c
ardalangh <rdghoorchian@gmail.com>
2023-01-21 15:53:39 -0800

    in mem h2 db added and teh entity added

--- src/main/java/com/ardylearnsspring/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/ardylearnsspring/currencyexchangeservice/CurrencyExchange.java
@@ -1,13 +1,23 @@
 package com.ardylearnsspring.currencyexchangeservice;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+    @Id
     private long id;
+    @Column(name="currency_from")
     private String from;
+    @Column(name="currency_to")
     private String to;
     private BigDecimal conversionMultiple;
+
+    private String environment;
 
     public CurrencyExchange() {
     }
