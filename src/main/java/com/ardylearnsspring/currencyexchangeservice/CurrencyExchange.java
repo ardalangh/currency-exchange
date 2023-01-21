a182fe76f8bdbb421dfd38becabebf620b4603e1
ardalangh <rdghoorchian@gmail.com>
2023-01-21 15:39:37 -0800

    set up the CurrencyExchange service

---
+++ src/main/java/com/ardylearnsspring/currencyexchangeservice/CurrencyExchange.java
@@ -0,0 +1,54 @@
+package com.ardylearnsspring.currencyexchangeservice;
+
+
+import java.math.BigDecimal;
+
+public class CurrencyExchange {
+    private long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+
+    public CurrencyExchange() {
+    }
+
+    public CurrencyExchange(long id, String from, String to, BigDecimal conversionMultiple) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionMultiple = conversionMultiple;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public BigDecimal getConversionMultiple() {
+        return conversionMultiple;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public void setConversionMultiple(BigDecimal conversionMultiple) {
+        this.conversionMultiple = conversionMultiple;
+    }
+}
