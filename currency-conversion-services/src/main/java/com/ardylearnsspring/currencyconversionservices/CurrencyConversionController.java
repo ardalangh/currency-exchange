f8b44df91cf2604ae86f1bb104c4fee702077dc4
ardalangh <rdghoorchian@gmail.com>
2023-01-21 16:27:55 -0800

    CurrencyConversion Service added with a hardcoded endpoint

---
+++ currency-conversion-services/src/main/java/com/ardylearnsspring/currencyconversionservices/CurrencyConversionController.java
@@ -0,0 +1,27 @@
+package com.ardylearnsspring.currencyconversionservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+        return new CurrencyConversion(
+                10001L,
+                from,
+                to,
+                quantity,
+                BigDecimal.valueOf(1),
+                BigDecimal.valueOf(1),
+                ""
+                );
+    }
+}
